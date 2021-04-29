6d1fd461cdcefbb9ec40d48e71b8df87958ef830
Paramjeet Dhiman <paramjeetdhiman1997@gmail.com>
2021-04-29 17:50:56 +0530

    add multer package for upload stuff

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,30 @@ const app = express();
 const Post = require("./api/modules/Post");
 const postsData = new Post();
 
+var multer = require("multer");
+
+function getExt(mimeType) {
+  switch (mimeType) {
+    case "image/png":
+      return ".png";
+    case "image/jpeg":
+      return ".jpeg";
+  }
+}
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+app.use(express.json());
+
 /// cors resolve
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -24,6 +48,19 @@ app.get("/api/posts/:post_id", (req, res) => {
   } else {
     res.status(404).send("Not Found");
   }
+});
+
+app.post("/api/posts", upload.single("post_image"), (req, res) => {
+  const newPost = {
+    id: `${Date.now()}`,
+    title: req.body.title,
+    content: req.body.content,
+    post_image: req.file.path,
+    added_date: `${Date.now()}`,
+  };
+  postsData.add(newPost);
+
+  res.status(200).send("ok");
 });
 
 app.listen(3000, () => {
